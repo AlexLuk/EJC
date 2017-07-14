ce29034fd0c47336b7267e5725bdfbe708e43b24
Alexandr <alexlukovkin@gmail.com>
2017-07-14 19:32:36 +0300

    Added method for counting unique symbols

--- task05/src/WorkWithStrings/Main.java
+++ task05/src/WorkWithStrings/Main.java
@@ -1,16 +1,38 @@
 package WorkWithStrings;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Main arraysTask = new Main();
         System.out.println(arraysTask.getMaxSameSymbols("qqqqqqew111111111111eefvs"));
+        System.out.println(arraysTask.countUniqueSymbols("abababababab"));
     }
 
     private int getMaxSameSymbols(String inputString) {
         int maxCounter = 0;
         inputString=inputString.replaceAll("\\d+","");
+        ArrayList<UniqueSymbolCounter> uniqueSymbolsArray = getUniqueSymbolCounters(inputString);
+
+        for (int i = 0; i < uniqueSymbolsArray.size(); i++) {
+            int uniqueSymbolsItemCounter = uniqueSymbolsArray.get(i).getCounter();
+            if (uniqueSymbolsItemCounter > maxCounter) {
+                maxCounter = uniqueSymbolsItemCounter;
+            }
+        }
+        return maxCounter;
+    }
+
+    int countUniqueSymbols(String inputString){
+        ArrayList<UniqueSymbolCounter> uniqueSymbolCounters = getUniqueSymbolCounters(inputString);
+        HashSet reallyUniqueSymbols=new HashSet();
+        reallyUniqueSymbols.addAll(uniqueSymbolCounters);
+        return reallyUniqueSymbols.size();
+    }
+
+    private ArrayList<UniqueSymbolCounter> getUniqueSymbolCounters(String inputString) {
         char[] symbols = inputString.toCharArray();
         ArrayList<UniqueSymbolCounter> uniqueSymbolsArray = new ArrayList<>();
         for (char symbol : symbols) {
@@ -28,15 +50,7 @@ public class Main {
                 }
             }
         }
-
-        for (int i = 0; i < uniqueSymbolsArray.size(); i++) {
-            int uniqueSymbolsItemCounter = uniqueSymbolsArray.get(i).getCounter();
-            if (uniqueSymbolsItemCounter > maxCounter) {
-                maxCounter = uniqueSymbolsItemCounter;
-            }
-        }
-
-        return maxCounter;
+        return uniqueSymbolsArray;
     }
 
 }
--- task05/src/WorkWithStrings/UniqueSymbolCounter.java
+++ task05/src/WorkWithStrings/UniqueSymbolCounter.java
@@ -4,8 +4,24 @@ class UniqueSymbolCounter {
     private char symbol;
     private int counter;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UniqueSymbolCounter that = (UniqueSymbolCounter) o;
+
+        return symbol == that.symbol;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) symbol;
+    }
+
     UniqueSymbolCounter(char symbol, int counter) {
         this.symbol = symbol;
+
         this.counter = counter;
     }
 

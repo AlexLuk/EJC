164c336d46b8f3d76cf879e0b2b5dd5f9f717633
Alexandr Lukovkin <alexlukovkin@gmail.com>
2017-07-27 17:41:54 +0300

    Added bubble sort with tests

--- task_08/src/sorting_algorithms/BubbleSorter.java
+++ task_08/src/sorting_algorithms/BubbleSorter.java
@@ -1,4 +1,21 @@
 package sorting_algorithms;
 
-public class BubbleSorter {
+public class BubbleSorter extends Sorter{
+    public static Integer[] bubbleSort(Integer[] inputArray) {
+        Integer[] resultArray = new Integer[inputArray.length];
+        copyArray(inputArray, resultArray);
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < resultArray.length - 1; i++) {
+                int currentElement = resultArray[i];
+                int nextElement = resultArray[i + 1];
+                if (currentElement > nextElement) {
+                    swapElements(resultArray,i,i+1);
+                    isSorted = false;
+                }
+            }
+        }
+        return resultArray;
+    }
 }
--- task_08/test/sorting_algorithms/BubbleSorterTest.groovy
+++ task_08/test/sorting_algorithms/BubbleSorterTest.groovy
@@ -2,5 +2,29 @@ package sorting_algorithms
 
 
 class BubbleSorterTest extends GroovyTestCase {
+    void testInsertionSort() {
+        Integer[] testArray = [4, 3, 2, 1];
+        Integer[] testSortedArray = [1, 2, 3, 4];
+        assertArrayEquals(testSortedArray, BubbleSorter.bubbleSort(testArray));
 
+        testArray = [1];
+        testSortedArray = [1];
+        assertArrayEquals(testSortedArray, BubbleSorter.bubbleSort(testArray));
+
+        testArray = [];
+        testSortedArray = [];
+        assertArrayEquals(testSortedArray, BubbleSorter.bubbleSort(testArray));
+
+        testArray = [1, 2, 3, 4];
+        testSortedArray = [1, 2, 3, 4];
+        assertArrayEquals(testSortedArray, BubbleSorter.bubbleSort(testArray));
+
+        testArray = [-2, -1, -4, -3];
+        testSortedArray = [-4, -3, -2, -1];
+        assertArrayEquals(testSortedArray, BubbleSorter.bubbleSort(testArray));
+
+        testArray = [-1, 2, 3, -4];
+        testSortedArray = [-4, -1, 2, 3];
+        assertArrayEquals(testSortedArray, BubbleSorter.bubbleSort(testArray));
+    }
 }

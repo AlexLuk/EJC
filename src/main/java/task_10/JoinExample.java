03e519af7890a5a454db145ac29b29faebbb280f
Alexandr <alexlukovkin@gmail.com>
2017-08-07 18:45:01 +0300

    Task 10 Threads examples. Join, Wait, Notify, Yeild

--- src/main/java/task_10/JoinExample.java
+++ src/main/java/task_10/JoinExample.java
@@ -1,4 +1,27 @@
 package task_10;
 
 public class JoinExample {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.err.println(i);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.err.println("start");
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("finish");
+    }
 }
--- src/main/java/task_10/YeildExample.java
+++ src/main/java/task_10/YeildExample.java
@@ -1,4 +1,57 @@
 package task_10;
 
 public class YeildExample {
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+
+            @Override
+
+            public void run() {
+
+                for (int i = 0; i < 100; i++) {
+
+                    System.err.println(Thread.currentThread().getName() + " " + i);
+
+//                    Thread.yield();
+
+                }
+
+            }
+
+        };
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+    }
 }

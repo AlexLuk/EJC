b88812c419358b13ce7b2773d50e94ff2f2954e6
Alexandr <alexlukovkin@gmail.com>
2017-08-10 16:25:13 +0300

    Added Javadoc to FileReaderThread Moved constants to ResourceHolder class

--- src/main/java/task_11/process_files_in_threads/FileReaderThread.java
+++ src/main/java/task_11/process_files_in_threads/FileReaderThread.java
@@ -4,9 +4,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * Class is designed to read file in separate thread
+ */
 public class FileReaderThread extends Thread {
-    public static final String WRONG_FORMAT_MESSAGE_PART1 = "Wrong line format in file: ";
-    public static final String WRONG_FORMAT_MESSAGE_PART2 = " line: ";
     private FileReader fileReader;
     private EntriesHolder entriesHolder;
     private ReportProcessor reportProcessor;
@@ -19,13 +20,19 @@ public class FileReaderThread extends Thread {
         this.reportProcessor = reportProcessor;
     }
 
+    /**
+     * Method is designed to read input file line after line
+     * For each readed line method parseString is called
+     * Before finishing thread it decreases thread counter in
+     * reportProcessor
+     */
     @Override
     public void run() {
         int symbol;
         try {
             StringBuilder stringBuilder = new StringBuilder();
             boolean isHeaderLine = true;
-            int lineCounter=1;
+            int lineCounter = 1;
             while ((symbol = fileReader.read()) != -1) {
                 if (symbol != '\n') {
                     if (symbol != '\r') {
@@ -35,7 +42,7 @@ public class FileReaderThread extends Thread {
                     if (isHeaderLine) {
                         isHeaderLine = false;
                     } else {
-                        parseString(stringBuilder.toString(),lineCounter);
+                        parseString(stringBuilder.toString(), lineCounter);
                     }
                     lineCounter++;
                     stringBuilder = new StringBuilder();
@@ -48,6 +55,12 @@ public class FileReaderThread extends Thread {
         reportProcessor.decreaseThreadCounter();
     }
 
+    /**
+     * Method is designed to parse loaded string and save it as entry in {@link EntriesHolder}
+     *
+     * @param inputString - string loaded from file
+     * @param lineNumber  - number loaded line in file
+     */
     void parseString(String inputString, int lineNumber) {
         String[] entryFields = inputString.split(ResourceHolder.ENTREE_SPLIT_REGEX);
         try {
@@ -55,7 +68,7 @@ public class FileReaderThread extends Thread {
                             entryFields[1]),
                     Integer.valueOf(entryFields[2]));
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            System.err.println(WRONG_FORMAT_MESSAGE_PART1 + fileName + WRONG_FORMAT_MESSAGE_PART2 + lineNumber);
+            System.err.println(ResourceHolder.WRONG_FORMAT_MESSAGE_PART1 + fileName + ResourceHolder.WRONG_FORMAT_MESSAGE_PART2 + lineNumber);
         }
     }
 }

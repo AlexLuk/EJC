c60fd017c70a918dc910a6245b30348aaa81c1c6
Alexandr <alexlukovkin@gmail.com>
2017-08-10 16:26:18 +0300

    Added Javadoc to EntriesHolder class Made some minor fixes of code-style

--- src/main/java/task_11/process_files_in_threads/EntriesHolder.java
+++ src/main/java/task_11/process_files_in_threads/EntriesHolder.java
@@ -2,11 +2,23 @@ package task_11.process_files_in_threads;
 
 import java.util.HashMap;
 
-public class EntriesHolder extends HashMap<UserSiteWrapper, Integer>{
+/**
+ * Class is designed to store information about duration of time
+ * user spend on exact site.
+ */
+public class EntriesHolder extends HashMap<UserSiteWrapper, Integer> {
 
+    /**
+     * Method it designed to update state of {@link EntriesHolder}.
+     * When we put value to existing key - new value is added to old one
+     *
+     * @param key   - the key
+     * @param value - time duration
+     * @return - new keu value pair
+     */
     @Override
     public synchronized Integer put(UserSiteWrapper key, Integer value) {
-        if(containsKey(key)){
+        if (containsKey(key)) {
             value = value + get(key);
         }
         return super.put(key, value);

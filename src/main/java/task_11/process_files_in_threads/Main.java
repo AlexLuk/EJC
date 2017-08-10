db8569c12a508867656f99c5798257bc0ff8ac0c
Alexandr <alexlukovkin@gmail.com>
2017-08-10 16:17:45 +0300

    Added Javadoc to Main class.

--- src/main/java/task_11/process_files_in_threads/Main.java
+++ src/main/java/task_11/process_files_in_threads/Main.java
@@ -2,6 +2,11 @@ package task_11.process_files_in_threads;
 
 public class Main {
 
+    /**
+     * Class is designed to start our application
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         ReportFilesGenerator reportFilesGenerator = new ReportFilesGenerator(15);

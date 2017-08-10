d7f005e33713118ca8b2cf7eefc94f394423a9ef
Alexandr <alexlukovkin@gmail.com>
2017-08-10 16:22:25 +0300

    Added JavaDOC to ResourceHolder Moved here some constant lines from FileReaderThread class

--- src/main/java/task_11/process_files_in_threads/ResourceHolder.java
+++ src/main/java/task_11/process_files_in_threads/ResourceHolder.java
@@ -1,5 +1,8 @@
 package task_11.process_files_in_threads;
 
+/**
+ * Class is designed to store list of constants for other classes
+ */
 public class ResourceHolder {
     //ReportProcessor settings
     static final String RESOURCE_FOLDER_PATH = "src\\main\\resources\\task_11\\";
@@ -18,12 +21,14 @@ public class ResourceHolder {
     final static String REPORT_NAME_TEMPLATE = "report";
     final static String REPORT_HEADER = "id,url,time,user\r\n";
     final static int NUM_OF_REPORT_FILES = 20;
-    final static int MIN_VISIT_DURATION = 4_000;
-    private final static int MAX_VISIT_DURATION = 100_000;
-    static final int VISIT_DURATION_INTERVAL = MAX_VISIT_DURATION - MIN_VISIT_DURATION;
     final static int MIN_REPORT_LENGTH = 10;
     final static int MAX_REPORT_LENGTH = 20;
+    final static int MIN_VISIT_DURATION = 4_000;
+    final static int MAX_VISIT_DURATION = 100_000;
+    final static int VISIT_DURATION_INTERVAL = MAX_VISIT_DURATION - MIN_VISIT_DURATION;
 
-    //ReportFilesGenerator settings
+    //FileReaderThread settings
+    final static String WRONG_FORMAT_MESSAGE_PART1 = "Wrong line format in file: ";
+    final static String WRONG_FORMAT_MESSAGE_PART2 = " line: ";
     static final String ENTREE_SPLIT_REGEX = "[" + COLUMN_SEPARATOR + "]";
 }
